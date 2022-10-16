97f82570ab1a92257b38f9d116ce9ce4db5a20c2
CheapNightbot <softtechnology512@gmail.com>
2022-10-17 01:28:46 +0530

    Updated README.md

--- Media-ShortKeys.py
+++ Media-ShortKeys.py
@@ -1,6 +1,12 @@
 import keyboard
 
 '''
+Keyboard event names are case sensitive as 'b' and 'B' both are different.
+Try running following code (where 1st will type 'd', but 2nd will mute system audio):
+
+keyboard.send("d")
+keyboard.send("D")
+----------------------------------
 Media Keys' keyboard event name:
 
 B = Volume up
@@ -12,6 +18,12 @@ P = Next Track
 J = Stop
 
 '''
+# ______________________________ #
+
+'''
+add_hotkey() function with anonymous lambda function.
+You can also add named functions.
+'''
 
 # Volume Up = Ctrl + Shift + ↑
 keyboard.add_hotkey('ctrl + shift + up', lambda: keyboard.send("B"), suppress=True)
@@ -22,18 +34,21 @@ keyboard.add_hotkey('ctrl + shift + m', lambda: keyboard.send("D"), suppress=Tru
 # Volume Down = Ctrl + Shift + ↓
 keyboard.add_hotkey('ctrl + shift + down', lambda: keyboard.send("C"), suppress=True)
 
-# Previous Track = Ctrl + Shift + ←
+# Previous = Ctrl + Shift + ←
 keyboard.add_hotkey('ctrl + shift + left', lambda: keyboard.send("Q"), suppress=True)
 
 # Play / Pause = Ctrl + Shift + Space
 keyboard.add_hotkey('ctrl + shift + space', lambda: keyboard.send("G"), suppress=True)
 
-# Next Track = Ctrl + Shift + →
+# Next = Ctrl + Shift + →
 keyboard.add_hotkey('ctrl + shift + right', lambda: keyboard.send("P"), suppress=True)
 
 # Stop = Ctrl + Alt + Space
 keyboard.add_hotkey('ctrl + alt + space', lambda: keyboard.send("J"), suppress=True)
 
+'''
+If the program finishes, the hotkey is not in effect anymore
+and program will close
+It will prevent the program from closing
+'''
 keyboard.wait()
-
-# pyinstaller -n "Media-ShortKeys" --noconsole --icon=keyboard.ico --add-data="keyboard.ico;." Media-ShortKeys.py
